3e281cc258e6eada6dd5cd7d49ed89c034ca9539
Xiaokang2022 <2951256653@qq.com>
2024-09-16 22:54:39 +0800

    Updated demo10

--- demos/demo10/main.py
+++ demos/demo10/main.py
@@ -15,15 +15,18 @@ class FPSVideoCanvas(media.VideoCanvas):
     def _refresh(self) -> None:
         global t
         now = time.time()
-        cv.itemconfigure(fps, text=f"FPS: {int(1/(now-t)):02d}")
+        try:
+            cv.itemconfigure(fps, text=f"FPS: {int(1/(now-t)):02d}")
+        except ZeroDivisionError:
+            pass
         t = now
         super()._refresh()
 
 
 root = tkt.Tk(title="tkintertools-media")
 t = time.time()
-cv = FPSVideoCanvas(root, keep_ratio="min",
-                    free_anchor=True, control=True, max_fps=60)
+cv = FPSVideoCanvas(root, keep_ratio="min", free_anchor=True,
+                    control=True, auto_play=True)
 fps = cv.create_text(20, 20, text="FPS: 0", anchor="nw", fill="#0F0")
 cv.place(width=1280, height=720, x=640, y=360, anchor="center")
 cv.play("./assets/videos/Bad Apple.mp4")
