f7de73c74168883fa8858017bfe63ca493853faf
Xiaokang2022 <2951256653@qq.com>
2024-09-20 21:03:57 +0800

    Updated `tkintertools-media` to v1.1.0

--- demos/demo10/main.py
+++ demos/demo10/main.py
@@ -1,5 +1,3 @@
-import time
-
 import tkintertools as tkt
 import tkintertools.core.constants as constants
 import tkintertools.media as media
@@ -8,26 +6,8 @@ import tkintertools.toolbox as toolbox
 if toolbox.load_font("./assets/fonts/LXGWWenKai-Regular.ttf"):
     constants.FONT = "霞鹜文楷"
 
-
-class FPSVideoCanvas(media.VideoCanvas):
-    """添加显示 FPS 的功能"""
-
-    def _refresh(self) -> None:
-        global t
-        now = time.time()
-        try:
-            cv.itemconfigure(fps, text=f"FPS: {int(1/(now-t)):02d}")
-        except ZeroDivisionError:
-            pass
-        t = now
-        super()._refresh()
-
-
-root = tkt.Tk(title="tkintertools-media")
-t = time.time()
-cv = FPSVideoCanvas(root, keep_ratio="min", free_anchor=True,
-                    control=True, auto_play=True)
-fps = cv.create_text(20, 20, text="FPS: 0", anchor="nw", fill="#0F0")
+root = tkt.Tk(title=f"tkintertools-media v{media.__version__}")
+cv = media.VideoCanvas(root, keep_ratio="min", free_anchor=True, controls=True)
 cv.place(width=1280, height=720, x=640, y=360, anchor="center")
-cv.play("./assets/videos/Bad Apple.mp4")
+cv.open("./assets/videos/Bad Apple.mp4")
 root.mainloop()
